032413a77c6e3784f009935931005269111338b0
Francisco <Fran89@users.noreply.github.com>
2018-10-30 23:04:47 -0400

    Add licence to header

--- Gsof2Ring.sh
+++ Gsof2Ring.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+
+#    Gsof2Ring uses PyEarthWorm to interface the gsof messages from trimble to the EW Transport system.
+#    Copyright (C) 2018  Francisco J Hernandez Ramirez
+#    You may contact me at FJHernandez89@gmail.com, FHernandez@boritechsolutions.com
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>
+
 from struct import unpack
 import struct
 import PyEW
